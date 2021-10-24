0e4a6f61b96568659ad188a78df2f565c40e10c5
MrDham <vincent.dhamelincourt@wanadoo.fr>
2021-10-24 11:54:29 +0200

    Add files via upload

--- Software/Open_Theremin_V4/timer.h
+++ Software/Open_Theremin_V4/timer.h
@@ -2,6 +2,7 @@
 #define _TIMER_H
 
 extern volatile uint16_t timer;
+extern volatile uint16_t midi_timer;
 
 inline uint16_t millisToTicks(uint16_t milliseconds) {
   return milliseconds * (1000.0f/32);
@@ -13,6 +14,10 @@ inline void resetTimer() {
 
 inline void incrementTimer() {
   timer++;
+}
+
+inline void incrementMidiTimer() {
+  midi_timer++;
 }
 
 inline bool timerExpired(uint16_t ticks) {
